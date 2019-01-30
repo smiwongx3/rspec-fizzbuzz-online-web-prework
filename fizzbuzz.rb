cbf6f816529cb67ceb7c0f2006832b1538f9bbce
Samantha Wong <smiwong920@gmail.com>
2019-01-30 22:04:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0 
+  if(int % 3 == 0) 
     "Fizz"
   end 
-  elsif int % 5 == 0 
+  elsif(int % 5 == 0)
     "Buzz" 
   end 
-  else int % 3 && 5 == 0 
+  else(int % 3 && 5 == 0)
     "FizzBuzz"
   end 
 end
